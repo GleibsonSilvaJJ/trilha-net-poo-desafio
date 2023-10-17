65bfea5e0c3adfed6f1edcee091b5f4981a866b6
gleibsonsilvajj <gleibsonfsilvadev@gmail.com>
2023-10-16 22:16:19 -0300

    Add as partes solicidatas no desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
-﻿using DesafioPOO.Models;
+﻿using System.ComponentModel;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "12345678", modelo: "Modelo 1", imei: "11111111111", memoria: 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Instagram ");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "88559655", modelo: "Modelo 2", imei: "222222222", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Telegram ");
